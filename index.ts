4d71038120a0b15f413f55a259ba679291b7ed3d
Gustavo Costa <gustavo.costa.w@gmail.com>
2017-08-02 15:58:19 -0300

    remove module file

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-export * from './module/ngc-float-button.component';
-export * from './module/ngc-float-item-button.component';
-export * from './module/ngc-float-button.module';
+import { NgcFloatButtonComponent } from './components/ngc-float-button.component';
+import { NgcFloatItemButtonComponent } from './components/ngc-float-item-button.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MaterialModule
+  ],
+  declarations: [NgcFloatButtonComponent, NgcFloatItemButtonComponent],
+  exports:[NgcFloatButtonComponent, NgcFloatItemButtonComponent]
+})
+
+export class NgcFloatButtonModule { }
