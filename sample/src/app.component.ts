40b4dc847aff34eabfb240d12533767bafb6998e
Gustavo Costa <gustavo.costa.w@gmail.com>
2017-08-03 17:59:15 -0300

    create sample to float button

--- sample/src/app.component.ts
+++ sample/src/app.component.ts
@@ -1,9 +1,17 @@
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ngc-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  private open:BehaviorSubject<boolean> = new BehaviorSubject(true);
+
+  private output(log) {
+    console.log(log);
+  }
 
 }
