3d72078d6cc43ef8e555bea51013763910f6d31f
Gustavo Costa <gustavo.costa.w@gmail.com>
2017-08-04 14:45:16 -0300

    organize space between imports

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import { NgcFloatButtonComponent } from './components/ngc-float-button.component';
-import { NgcFloatItemButtonComponent } from './components/ngc-float-item-button.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@angular/material';
+
+import { NgcFloatButtonComponent } from './components/ngc-float-button.component';
+import { NgcFloatItemButtonComponent } from './components/ngc-float-item-button.component';
 
 @NgModule({
   imports: [
