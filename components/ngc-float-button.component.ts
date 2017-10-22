d56145399e05a8f99654b3bb0a0e768ed26f8d6f
Gustavo Costa <gustavo.costa.w@gmail.com>
2017-10-22 21:48:54 -0200

    update md to mat components

--- components/ngc-float-button.component.ts
+++ components/ngc-float-button.component.ts
@@ -22,11 +22,76 @@ import {
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'ngc-float-button',
-  styleUrls: ['./ngc-float-button.component.css'],
+  styles: [`
+
+  :host {
+    position: absolute;
+  }
+
+  .fab-menu {
+      box-sizing: border-box;
+      font-size: 20px;
+      width:55px;
+      height: 55px;
+      text-align: left;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      z-index: 9;
+  }
+
+  .fab-toggle {
+    background: #dd0031;
+    border-radius: 100%;
+    width: 40px;
+    height: 40px;
+    color: white;
+    text-align: center;
+    line-height: 50px;
+    transform: translate3d(0, 0, 0);
+    transition: all ease-out 200ms;
+    z-index: 2;
+    transition-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    transition-duration: 400ms;
+    transform: scale(1.1, 1.1) translate3d(0, 0, 0);
+    cursor: pointer;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.26);
+  }
+
+  .fab-menu .fab-toggle:hover {
+    transform: scale(1.2, 1.2) translate3d(0, 0, 0);
+  }
+
+  .fab-menu /deep/ .item {
+     opacity: 0;
+  }
+
+  .fab-menu.active /deep/ .item {
+     opacity: 1;
+  }
+
+  .fab-menu.active /deep/ .content-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .fab-menu.active /deep/ .content {
+    display: block;
+  }
+
+  .fab-menu.active .fab-toggle {
+    transition-timing-function: linear;
+    transition-duration: 200ms;
+    transform: scale(0.8, 0.8) translate3d(0, 0, 0);
+  }
+
+  `],
   template: `
     <nav class="fab-menu" [class.active]="state.getValue().display">
         <a class="fab-toggle" (click)="toggle()">
-          <md-icon class="material-content-icon"> {{icon}} </md-icon>
+          <mat-icon> {{icon}} </mat-icon>
         </a>
         <ng-content></ng-content>
     </nav>
@@ -109,16 +174,25 @@ export class NgcFloatButtonComponent implements AfterContentInit, OnDestroy, OnC
 
   // get transition direction
   private getTranslate(i) {
+
+      let animation;
+
       switch (this.direction) {
         case 'right' :
-           return `translate3d(${ this.state.getValue().spaceBetweenButtons * i }px,0,0)`;
+           animation = `translate3d(${ this.state.getValue().spaceBetweenButtons * i }px,0,0)`;
+           break;
         case  'bottom' :
-           return `translate3d(0,${ this.state.getValue().spaceBetweenButtons * i }px,0)`;
+          animation = `translate3d(0,${ this.state.getValue().spaceBetweenButtons * i }px,0)`;
+          break;
         case 'left' :
-           return `translate3d(-${ this.state.getValue().spaceBetweenButtons * i }px,0,0)`;
+          animation = `translate3d(-${ this.state.getValue().spaceBetweenButtons * i }px,0,0)`;
+          break;
         default :
-           return `translate3d(0,-${ this.state.getValue().spaceBetweenButtons * i }px,0)`;
+          animation = `translate3d(0,-${ this.state.getValue().spaceBetweenButtons * i }px,0)`;
+          break;
       }
+
+      return animation;
   }
 
   /* some problems here */
--- components/ngc-float-item-button.component.ts
+++ components/ngc-float-item-button.component.ts
@@ -11,14 +11,64 @@ import {
 
 @Component({
   selector: 'ngc-float-item-button',
-  styleUrls: ['./ngc-float-item-button.component.css'],
+  styles: [`
+
+  .item {
+    width:250px;
+    height: 40px;
+    left:-203px;
+    transform: translate3d(0, 0, 0);
+    transition: transform, opacity ease-out 200ms;
+    transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1);
+    transition-duration: 180ms;
+    position: absolute;
+    cursor: pointer;
+    top:5px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .content {
+    padding:5px;
+    background: #333333;
+    margin-right: 50px;
+    line-height: 25px;
+    color: white;
+    text-transform: lowercase;
+    padding:2px 7px;
+    border-radius: 3px;
+    display: none;
+    font-size: 12px;
+    height: 25px;
+    margin-top: 4px;
+    display: none;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.26);
+  }
+
+  .fab-item {
+    right: 0px;
+    background: white;
+    border-radius: 100%;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    color: #797979;
+    text-align: center;
+    cursor: pointer;
+    line-height: 50px;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.26);
+  }
+
+
+  `],
   template: `
     <div #elementref class="item" (click)="clicked.emit($event)">
         <div class="content-wrapper" #contentref>
           <div class="content" [style.display]="content ? 'block' : 'none'">{{content}}</div>
         </div>
         <a class="fab-item">
-           <md-icon class="material-content-icon"> {{icon}} </md-icon>
+           <mat-icon> {{icon}} </mat-icon>
         </a>
     </div>
   `,
