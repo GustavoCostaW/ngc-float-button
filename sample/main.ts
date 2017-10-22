69f0f250e993d4722ffa23f8ee5d174a04413caf
Gustavo Costa <gustavo.costa.w@gmail.com>
2017-10-22 21:49:03 -0200

    remove sample folder

--- sample/main.ts
+++
@@ -1,5 +0,0 @@
-import './polyfills.ts';
-import { AppModule } from './src/app.module';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
